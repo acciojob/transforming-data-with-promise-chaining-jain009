196c6445ac76cdec87a3a0cddc7778ef1b8c740f
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-08 23:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
 //your JS code here. If required.
+document.getElementById("ip").addEventListner("click", function(){
+	const outputDiv = document.getElementById("output");
+	let num = parseFloat(document.getElementById("ip").value);
+	if(isNaN(num)){
+		outputDiv.textContent = "Please enter a valid number.";
+		return;
+	}
+	new Promise((resolve => {
+	setTimeOut(() => {
+		outputDiv.textContent = `Result: ${num}`
+		resolve(num);
+	})
+	},2000);
+		.then(result => new Promise(resolve => {
+		setTimeout(() => {
+			result *= 2;
+			outputDiv.textContent = `Result: ${result}`
+			resolve(result);
+		}, 1000);
+	}))
+	.then(result => new Promise(resolve => {
+			setTimeout(() => {
+				result -= 3;
+				outputDiv.tectContent = `Result: ${result}`;
+				resolve(result);
+			}, 1000);
+	}))
+		.then(result  => new Promise(resolve => {
+				setTimeOut(() => {
+					result /= 2;
+					outputDiv.textContent =`Result: ${result}`;
+					resolve(result);
+				}, 1000);
+		}))
+		.then(result => new Promise(resolve =>{
+		setTimeOut(()  => {
+			result += 10;
+			outputdiv.textContent = `Final Result: ${result}`;
+			resolve(result);
+		},1000);
+		
+		}));
+				 }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		}))
+	}))
+})
