b58a38a892273646fc747e6769736573f1b9cf71
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-09 00:04:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,7 +38,44 @@ document.getElementById("ip").addEventListener("click", function(){
 			result += 10;
 			outputdiv.textContent = `Final Result: ${result}`;
 			resolve(result);
+		},1000);document.getElementById("btn").addEventListener("click", function(){
+	const outputDiv = document.getElementById("output");
+	let num = parseFloat(document.getElementById("ip").value);
+	if(isNaN(num)){
+		outputDiv.textContent = "Please enter a valid number.";
+		return;
+	}
+	new Promise(resolve => {
+		setTimeout(() => {
+			outputDiv.textContent = `Result: ${num}`;
+			resolve(num);
+		}, 2000);
+	}).then(result => new Promise(resolve => {
+		setTimeout(() => {
+			result *= 2;
+			outputDiv.textContent = `Result: ${result}`;
+			resolve(result);
+		}, 2000);
+	})).then(result => new Promise(resolve => {
+		setTimeout(() => {
+			result -= 3;
+			outputDiv.textContent = `Result: ${result}`;
+			resolve(result);
+		}, 1000);
+	})).then(result  => new Promise(resolve => {
+		setTimeout(() => {
+			result /= 2;
+			outputDiv.textContent = `Result: ${result}`;
+			resolve(result);
+		}, 1000);
+	})).then(result => new Promise(resolve =>{
+		setTimeout(()  => {
+			result += 10;
+			outputDiv.textContent = `Final Result: ${result}`;
+			resolve(result);
 		},1000);
+	}));
+});
 		
 		}));
 				 }));
